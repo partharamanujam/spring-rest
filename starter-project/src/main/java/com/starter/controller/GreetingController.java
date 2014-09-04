d7e0e8858e03ab663fb2376b0ee650043ac4fee6
Partha R <parthasarathi.ramanujam@gmail.com>
2014-09-04 17:50:33 +0530

    added log4j logging support

--- starter-project/src/main/java/com/starter/controller/GreetingController.java
+++ starter-project/src/main/java/com/starter/controller/GreetingController.java
@@ -2,6 +2,8 @@ package com.starter.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,12 +14,14 @@ import com.starter.model.Greeting;
 
 @Controller
 public class GreetingController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
 	public @ResponseBody Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		logger.info("GET: /greeting");
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
